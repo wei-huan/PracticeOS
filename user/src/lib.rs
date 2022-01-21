3e7baa6c3f4ad55c8b562ee369e4a3eb33367b09
wei-huan <1520799054@qq.com>
2022-01-21 20:39:45 +0800

    add apps

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -1,21 +1,37 @@
+#![no_std]
 #![feature(linkage)]
+#![feature(panic_info_message)]
+
+#[macro_use]
+pub mod console;
+mod syscall;
+mod lang_items;
+
 #[no_mangle]
 #[link_section = ".text.entry"]
-
 pub extern "C" fn _start() -> ! {
-
     clear_bss();
-
     exit(main());
-
     panic!("unreachable after sys_exit!");
+}
 
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|addr| {
+        unsafe { (addr as *mut u8).write_volatile(0); }
+    });
 }
 
 #[linkage = "weak"]
 #[no_mangle]
 fn main() -> i32 {
-
     panic!("Cannot find main!");
-
 }
+
+use syscall::*;
+
+pub fn write(fd: usize, buf: &[u8]) -> isize { sys_write(fd, buf) }
+pub fn exit(exit_code: i32) -> isize { sys_exit(exit_code) }
