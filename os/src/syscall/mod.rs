47c8213b8945b86bce16d017d9e1a72f696f602b
wei-huan <1520799054@qq.com>
2022-02-16 19:39:10 +0800

    修改部分源码

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -10,14 +10,14 @@ mod priority;
 
 use fs::*;
 use process::*;
-use priority::*;
+// use priority::*;
 
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
         SYSCALL_YIELD => sys_yield(),
-        SYSCALL_SET_PRIORITY => sys_set_priority(args[0] as isize),
+        // SYSCALL_SET_PRIORITY => sys_set_priority(args[0] as isize),
         SYSCALL_GET_TIME => sys_get_time(args[0] as *mut TimeVal, args[1]),
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
     }
--- os/src/syscall/priority.rs
+++ os/src/syscall/priority.rs
@@ -1,5 +1,5 @@
-use crate::task::set_current_priority;
+// use crate::task::set_current_priority;
 
-pub fn sys_set_priority(prio: isize) -> isize {
-    set_current_priority(prio)
-}
+// pub fn sys_set_priority(prio: isize) -> isize {
+//     set_current_priority(prio)
+// }
