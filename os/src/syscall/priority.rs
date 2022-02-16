1bf216b61264312df2dce6012ee0c78833ede20d
wei-huan <1520799054@qq.com>
2022-02-16 19:51:39 +0800

    能够正常运行新系统

--- os/src/syscall/priority.rs
+++
@@ -1,5 +0,0 @@
-// use crate::task::set_current_priority;
-
-// pub fn sys_set_priority(prio: isize) -> isize {
-//     set_current_priority(prio)
-// }
--- user/src/lang_items.rs
+++ user/src/lang_items.rs
@@ -1,5 +1,3 @@
-use crate::exit;
-
 #[panic_handler]
 fn panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
     let err = panic_info.message().unwrap();
@@ -13,5 +11,5 @@ fn panic_handler(panic_info: &core::panic::PanicInfo) -> ! {
     } else {
         println!("Panicked: {}", err);
     }
-    exit(-1);
+    loop {}
 }
