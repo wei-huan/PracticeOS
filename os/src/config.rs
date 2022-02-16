9fe9a7d9b1d6a1978288081fc855304bd98b11f0
wei-huan <1520799054@qq.com>
2022-02-16 20:47:53 +0800

    添加注释

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,8 +1,16 @@
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
+
+// 3MiB 的内核堆区
 pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
+
+// k210 Flash内存一共 8MiB, 从 0x80000000 到 0x80800000
 pub const MEMORY_END: usize = 0x80800000;
+
+// 一页 4KiB
 pub const PAGE_SIZE: usize = 0x1000;
+
+// 页内地址占据12bit位
 pub const PAGE_SIZE_BITS: usize = 0xc;
 
 pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
